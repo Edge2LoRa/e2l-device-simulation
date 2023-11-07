060dddf50b3afd3e5216a6874422f758b0db602d
carugno <96237399+carugno@users.noreply.github.com>
2023-11-07 12:57:54 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,131 @@
+const { packetGenerator } = require('./packetGenerator');
+const { promisify } = require('util');
+const fs = require('fs');
+const sleep = promisify(setTimeout);
+const dgram = require('dgram');
+const { getRandomValues } = require('crypto');
+const FCnt = 1 //or incremental
+
+const socketCreator = (host, port) => {
+    return new Promise((resolve, reject) => {
+        if (port > 0 && port < 65536) {
+            const socket = dgram.createSocket('udp4');
+            socket.connect(Number(port), host, (err) => {
+                if(err){
+                    console.log(err)
+                    return reject(err)
+                }else{
+                    return resolve(socket)
+                }});
+        } else {
+            return reject(`Invalid port number: ${port}`);
+        }
+    })
+}
+
+
+const sendPacketToAllGWs = (packet, socket_arrays) => {
+    for (const socket of socket_arrays) {
+        if (socket) {
+            socket.send(packet, 0, packet.length);
+        } else {
+            console.error("Invalid socket in the array:", socket);
+        }
+    }
+};
+
+
+function simulateDevice(DevAddr, AppSKey, NwkSKey, FPort, FCnt, sleepTimer, nPackets, frameLoss, socket_arrays) {
+    return new Promise(async (resolve, reject) => {
+        await sleep(Math.floor(Math.random() * 5 ) + 5)
+        while( FCnt  < nPackets) {
+            const packet = packetGenerator(DevAddr, AppSKey, NwkSKey, FPort, FCnt);
+            // Generate a random number between 0 and 9
+            const randomValue = Math.floor(Math.random() * 10);
+            // Send the packet if randomValue is less than 8, discard otherwise
+            if (randomValue < frameLoss) {
+                sendPacketToAllGWs(packet, socket_arrays);
+                console.log(`Packet sent with DevAddr: ${DevAddr}, FCnt: ${FCnt}`)
+            }
+            FCnt = FCnt + 1
+            await sleep(sleepTimer); //4)duty cycle che andrà passato quando si chiama simulateDevice, o sleep(rand(start, end))
+        }
+        return resolve("Everything Alright");//5)qui inserirei l'attivazione del device, ma forse ho capito male l'assignment
+    });
+}
+
+function main(){
+    // Read the JSON file containing gateway information
+    const gatewayData = JSON.parse(fs.readFileSync('experiment_files/gateways.json'));
+    // Read the JSON file containing experiment information
+    const experimentData = JSON.parse(fs.readFileSync('experiment_files/experiment.json'));
+    const experiment = experimentData.experiment[0]; // Assuming there's only one experiment in the array
+    const ratio = experiment.ratio;
+    const sleepTimer = experiment.sleepTimer;
+    const deviceNumber = experiment.deviceNumber;
+    const minPacket = experiment.minPacket;
+    const maxPacket = experiment.maxPacket;
+    const frameLoss = experiment.frameLoss;
+    const deviceList = experiment.deviceList;
+    // Read the JSON file containing device information
+    const deviceData = JSON.parse(fs.readFileSync(deviceList));
+    let promise_socket_arrays = [];
+    for (const gateway of gatewayData.gateways) {
+        promise_socket_arrays.push(socketCreator(gateway.host, gateway.port))
+    }
+    Promise.all(promise_socket_arrays).then((socket_arrays) => {
+        let promise_device_arrays = []
+        for(const index in deviceData) {
+            if (index >= deviceNumber){
+                break;
+            }
+            const device = deviceData[index];
+            const DevAddr = device.session.dev_addr;
+            const AppSKey = device.session.keys.app_s_key.key;
+            const NwkSKey = device.session.keys.f_nwk_s_int_key.key;
+            //ratio used is legacy/edge, it means 1 leagacy every n edge devices
+            currentRatio = 0;
+            if (currentRatio === ratio) {
+                FPort = 2 //legacy
+                currentRatio = 0
+            }else{
+                FPort = 4 //edge devo rimette 4 poi
+                currentRatio ++
+            }
+            // Generate a random number between FCnt and FCnt + 10
+            const nPackets = Math.floor(Math.random() * (maxPacket - minPacket)) + minPacket;
+            promise_device_arrays.push(simulateDevice(DevAddr, AppSKey, NwkSKey, FPort, FCnt, sleepTimer, nPackets, frameLoss, socket_arrays))
+        }
+        Promise.all(promise_device_arrays).then(() => {
+            console.log("Experiment Ended Successfully")
+        }).catch((err) => {
+            console.error(err)
+        }).finally(() => {
+            for(socket of socket_arrays){
+                socket.disconnect()
+            }
+        })
+    })
+    
+}
+main();
+
+/*Improvements:
+    -Dare al main:
+        1)numero di pacchetti totali da inviare e renderlo variabile
+        2)quanti devices totali creare
+        3)quanti devices devono essere edge/legacy -> Potremmo modificare il ciclo for dandogli due parametri i e j, 
+        dove i+j sono il numero di device totali che estraggo dal json, i primi device li setta con FPort 2, dopo i iterazioni setta i successivi con FPort a 4
+        3.1)ho cambiato la logica del 3) usando un ratio legacy/edge
+        4)settare duty cycle
+        5)periodo di attivazione di ogni device
+    Tutto questo potrebbe essere inserito in un json oppure in una funzione specifica per ogni esperimento cosi da rendere modulare
+*/
+
+
+/*
+
+Da fare, ogni volta che genero il pacchetto funzione random da 0 a 9, se minore di 8 lo mando, se maggiore o = lo scarto
+
+entro nei 2 rasp, accendo i 2 proxy e mi mando i pacchetti e mi metto su nova per mandare i pacchetti
+*/
---
+++ packetGenerator.js
@@ -0,0 +1,61 @@
+const lora_packet = require("lora-packet");
+
+ function packetGenerator(DevAddr, AppSKey, NwkSKey, FPort, FCnt) {
+    // Construct the LoRa packet
+    const constructedPacket = lora_packet.fromFields(
+      {
+        FPort: FPort, //FPort = 4 Device edge / FPort = 2 Device Legacy
+        MType: "Unconfirmed Data Up",
+        DevAddr: Buffer.from(DevAddr, "hex"),
+        FCtrl: {
+          ADR: false,
+          ACK: false,
+          ADRACKReq: false,
+          FPending: false,
+        },
+        FCnt: FCnt, //counter
+        payload: "test", // Replace with your payload
+      },
+      Buffer.from(AppSKey, "hex"),
+      Buffer.from(NwkSKey, "hex")
+    );
+    const payloadBase64 = constructedPacket.getPHYPayload().toString("base64");
+    const size = payloadBase64.length;
+    let jsonUDP = {
+      rxpk: [
+        {
+          tmst: 2933348684,
+          chan: 7,
+          rfch: 0,
+          freq: 867.9,
+          stat: 1,
+          modu: 'LORA',
+          datr: 'SF7BW125',
+          codr: '4/5',
+          lsnr: 9.2,
+          rssi: -30,
+          size: size,
+          data: payloadBase64,
+        }
+      ]
+    };
+    jsonPacket = JSON.stringify(jsonUDP);
+    /*headerPKTFWD[0] == PROTOCOL_VERSION == 2
+    headerPKTFWD[1] == numero random
+    headerPKTFWD[2] == numero random
+    headerPKTFWD[3] == PKT_PUSH_DATA == 0
+    headerPKTFWD[4] == net_mac_h = htonl((uint32_t)(0xFFFFFFFF & (lgwm>>32)));
+    headerPKTFWD[8] == net_mac_l = htonl((uint32_t)(0xFFFFFFFF &  lgwm  ));
+    lgwm == 0 Lora gateway MAC address
+    */
+    let headerPKTFWD = new Uint8Array([2, 45, 141, 0, 184, 39, 235, 255, 254, 230, 15, 44]);
+    let enc = new TextEncoder();
+    let json = enc.encode(jsonPacket);
+
+    // Create a new array with the total length and merge all source arrays.
+    let mergedArray = new Uint8Array(headerPKTFWD.length + json.length);
+    mergedArray.set(headerPKTFWD, 0); // Copy headerPKTFWD to mergedArray at the beginning.
+    mergedArray.set(json, headerPKTFWD.length); // Copy json to mergedArray after headerPKTFWD.   
+    return mergedArray;
+}
+module.exports = { packetGenerator };
