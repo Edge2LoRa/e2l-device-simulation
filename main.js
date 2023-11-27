bf33c8d399d5c07e1b0649e03f74e7445f942770
carugno <96237399+carugno@users.noreply.github.com>
2023-11-27 14:51:12 +0100

    multigw loss function
    modified the loss function, now supports the multigateways

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const sleep = promisify(setTimeout);
 const dgram = require('dgram');
 const { getRandomValues } = require('crypto');
-const FCnt = 1 //or incremental
+const FCnt = 1 //Frame Counter Initialisation
 let activeDevices = 0;
 
 const socketCreator = (host, port) => {
@@ -24,27 +24,39 @@ const socketCreator = (host, port) => {
     })
 }
 function calculateLoss() {
-    const P = 20; // pkt size in byte
-    const s = 1 / 90; // pkt/sec
-  
+    const P = 20; // Packet size in byte
+
+    // every ED generates packets with a source rate of s pkt/s
+    const s = 1 / 90; // [pkt/sec]
+    // nodes number
+    const N = activeDevices;
+
     const SF_array = [7, 8, 9, 10, 11, 12];
+    // selected spreading factor
     const SF_index = 0;
+    // bandwidth in hertz
     const BW = 125000;
+    // coding rate CR = 4=(4 + RDD)
+    // with the number of redundancy bits RDD = 1; · · · ; 4.
     const CR = 1;
+    // time interval required for transmitting a packet
     const ToA = 0.0566; // toa_calc(SF_array[SF_index], BW, P, CR);
-  
-    const G = activeDevices * ToA * s;
-  
+
+    let G = N * s * ToA; // normalized offered load % traffico offerto normalizzato
+
+    // Number of query GW
+    const M = 2;
+
+    G = G / M;
+
     const eta = 2.9;
     const sir = 4; // 2.7; // 0.5; 2.7-->6dB CC in lorasim
     const alpha = Math.pow(10, sir / 10 / eta);
-    const Sc =
-      (1 / (2 * Math.pow(alpha, 2))) * (1 - Math.exp(-2 * G)) +
-      G * (1 - 1 / Math.pow(alpha, 2)) * Math.exp(-2 * G);
-  
-    const DER_CC = Sc / G;
+
+    const Sc = (1 / (2 * Math.pow(alpha, 2))) * (1 - Math.exp(-2 * G)) + G * (1 - 1 / Math.pow(alpha, 2)) * Math.exp(-2 * G);
+
+    const DER_CC = M * (Sc / G / M); // data extraction rate
     const loss = (1 - DER_CC) * 100; // perdita in percentuale
-  
     return loss;
   }
   
@@ -74,7 +86,7 @@ function simulateDevice(DevAddr, AppSKey, NwkSKey, FPort, FCnt, sleepTimer, nPac
             const frameLoss = calculateLoss(); // Calculate frame loss using activeDevices
             console.log(frameLoss);    
             sendPacketToAllGWs(packet,frameLoss, socket_arrays);
-            console.log(`Packet sent with DevAddr: ${DevAddr}, FCnt: ${FCnt}`)
+            console.log(`Packet sent with DevAddr: ${DevAddr}, FCnt: ${FCnt}, Devices: ${activeDevices}`)
             FCnt = FCnt + 1
             await sleep(sleepTimer); 
         }
@@ -95,7 +107,6 @@ function main(){
     const deviceNumber = experiment.deviceNumber;
     const minPacket = experiment.minPacket;
     const maxPacket = experiment.maxPacket;
-    //const frameLoss = experiment.frameLoss; // prima del ciclo for parto da questo frameloss, poi lo modifico prima di simulare ogni device
     const deviceList = experiment.deviceList;
     // Read the JSON file containing device information
     const deviceData = JSON.parse(fs.readFileSync(deviceList));
