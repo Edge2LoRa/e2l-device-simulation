e5cf6d02215bbd5da190d535162d91d8f71d336f
StefanoMilani <milani@diag.uniroma1.it>
2024-06-21 14:37:08 +0200

    FIX device simulation using dataset
    Signed-Off-By: Stefano Milani <milani@diag.uniroma1.it>

--- mainrealdata.js
+++ mainrealdata.js
@@ -1,177 +1,279 @@
-const { packetGeneratorReal } = require('./packetGeneratorReal');
-const { promisify } = require('util');
-const fs = require('fs');
+const { packetGeneratorReal } = require("./packetGeneratorReal");
+const { promisify } = require("util");
+const fs = require("fs");
 const sleep = promisify(setTimeout);
-const dgram = require('dgram');
-const { getRandomValues } = require('crypto');
-const FCnt = 1 //Frame Counter Initialisation
+const dgram = require("dgram");
+const { getRandomValues } = require("crypto");
+const { type } = require("os");
+const FCnt = 1; //Frame Counter Initialisation
 let activeDevices = 0;
 
 const socketCreator = (host, port) => {
-    return new Promise((resolve, reject) => {
-        if (port > 0 && port < 65536) {
-            const socket = dgram.createSocket('udp4');
-            socket.connect(Number(port), host, (err) => {
-                if(err){
-                    console.log(err)
-                    return reject(err)
-                } else {
-                    return resolve(socket)
-                }
-            });
+  return new Promise((resolve, reject) => {
+    if (port > 0 && port < 65536) {
+      const socket = dgram.createSocket("udp4");
+      socket.connect(Number(port), host, (err) => {
+        if (err) {
+          console.log(err);
+          return reject(err);
         } else {
-            return reject(`Invalid port number: ${port}`);
+          return resolve(socket);
         }
-    })
-}
-function calculateLoss() {
-    const P = 20; // Packet size in byte
-
-    // every ED generates packets with a source rate of s pkt/s
-    const s = 1 / 90; // [pkt/sec]
-    // nodes number
-    const N = activeDevices;
-
-    const SF_array = [7, 8, 9, 10, 11, 12];
-    // selected spreading factor
-    const SF_index = 0;
-    // bandwidth in hertz
-    const BW = 125000;
-    // coding rate CR = 4=(4 + RDD)
-    // with the number of redundancy bits RDD = 1; · · · ; 4.
-    const CR = 1;
-    // time interval required for transmitting a packet
-    const ToA = 0.0566; // toa_calc(SF_array[SF_index], BW, P, CR);
-
-    let G = N * s * ToA; // normalized offered load % traffico offerto normalizzato
-
-    // Number of query GW
-    const M = 2;
-
-    G = G / M;
-
-    const eta = 2.9;
-    const sir = 4; // 2.7; // 0.5; 2.7-->6dB CC in lorasim
-    const alpha = Math.pow(10, sir / 10 / eta);
-
-    const Sc = (1 / (2 * Math.pow(alpha, 2))) * (1 - Math.exp(-2 * G)) + G * (1 - 1 / Math.pow(alpha, 2)) * Math.exp(-2 * G);
-
-    const DER_CC = M * (Sc / G / M); // data extraction rate
-    const loss = (1 - DER_CC) * 100; // perdita in percentuale
-    return loss;
-  }
-  
-
-  const sendPacketToAllGWs = (packet, frameLoss, socket_arrays) => {
-    for (const socket of socket_arrays) {
-        if (socket) {
-            const randomValue = Math.floor(Math.random() * 100);
-            if (randomValue > frameLoss) {
-                socket.send(packet, 0, packet.length);
-            }
-        } else {
-            console.error("Invalid socket in the array:", socket);
-        }
+      });
+    } else {
+      return reject(`Invalid port number: ${port}`);
     }
+  });
+};
+function calculateLoss() {
+  const P = 20; // Packet size in byte
+
+  // every ED generates packets with a source rate of s pkt/s
+  const s = 1 / 90; // [pkt/sec]
+  // nodes number
+  const N = activeDevices;
+
+  const SF_array = [7, 8, 9, 10, 11, 12];
+  // selected spreading factor
+  const SF_index = 0;
+  // bandwidth in hertz
+  const BW = 125000;
+  // coding rate CR = 4=(4 + RDD)
+  // with the number of redundancy bits RDD = 1; · · · ; 4.
+  const CR = 1;
+  // time interval required for transmitting a packet
+  const ToA = 0.0566; // toa_calc(SF_array[SF_index], BW, P, CR);
+
+  let G = N * s * ToA; // normalized offered load % traffico offerto normalizzato
+
+  // Number of query GW
+  const M = 2;
+
+  G = G / M;
+
+  const eta = 2.9;
+  const sir = 4; // 2.7; // 0.5; 2.7-->6dB CC in lorasim
+  const alpha = Math.pow(10, sir / 10 / eta);
+
+  const Sc =
+    (1 / (2 * Math.pow(alpha, 2))) * (1 - Math.exp(-2 * G)) +
+    G * (1 - 1 / Math.pow(alpha, 2)) * Math.exp(-2 * G);
+
+  const DER_CC = M * (Sc / G / M); // data extraction rate
+  const loss = (1 - DER_CC) * 100; // perdita in percentuale
+  return loss;
+}
+
+const sendPacketToAllGWs = (packet, frameLosses, socket_arrays) => {
+  for (const index in socket_arrays) {
+    const socket = socket_arrays[index];
+    let frameLoss;
+    if (frameLosses instanceof Array)
+      frameLoss = frameLosses[index % frameLosses.length];
+    else frameLoss = frameLosses;
+    if (socket) {
+      const randomValue = Math.floor(Math.random() * 100);
+      if (randomValue > frameLoss) {
+        socket.send(packet, 0, packet.length);
+      }
+    } else {
+      console.error("Invalid socket in the array:", socket);
+    }
+  }
 };
 
+function simulateDevice(
+  DevAddr,
+  AppSKey,
+  NwkSKey,
+  FPort,
+  FCnt,
+  sleepTimer,
+  nPackets,
+  frameLoss_value,
+  socket_arrays,
+  packetsToSend
+) {
+  return new Promise(async (resolve, reject) => {
+    activeDevices++; // Increment activeDevices when simulating a device
 
-function simulateDevice(DevAddr, AppSKey, NwkSKey, FPort, FCnt, sleepTimer, nPackets, socket_arrays,deviceList){
-    return new Promise(async (resolve, reject) => {
-        // Load and parse the JSON file
-        const deviceData = JSON.parse(fs.readFileSync(deviceList));
+    for (const index in packetsToSend) {
+      const packetData = packetsToSend[index % packetsToSend.length];
+      if (FCnt >= nPackets) {
+        break;
+      }
+      //encode the payload
+      console.log(packetData.soil_temp);
+      const array = [
+        parseFloat(packetData.soil_temp),
+        parseFloat(packetData.soil_hum),
+      ];
+      const payload = Buffer.from(JSON.stringify(array));
+      const packet = packetGeneratorReal(
+        DevAddr,
+        AppSKey,
+        NwkSKey,
+        FPort,
+        FCnt,
+        packetData.data_rate,
+        packetData.coding_rate,
+        packetData.gtw_channel,
+        packetData.gtw_rssi,
+        packetData.gtw_snr,
+        payload
+      );
 
-        // Find the device in the JSON data that matches DevAddr
-        const device = deviceData.find(d => d.deviceid === DevAddr);
-        if (!device) {
-            console.log(`No device found with deviceid ${DevAddr}. Simulation ends for this device.`);
-            return resolve("Device not found");
-        }
-        
+      let frameLosses = [];
+      switch (frameLoss_value) {
+        case -2: // MOBILITY
+          // let gw_number = socket_arrays.length;
+          let divisor = 3;
+          let chunk_len = nPackets / divisor;
+          let chunk_index = Math.floor(FCnt / chunk_len);
 
-        activeDevices++; // Increment activeDevices when simulating a device
-        await sleep(Math.floor(Math.random() * 5 ) + 5)
-        const packetsToSend = deviceData.filter(d => d.deviceid === DevAddr);
-        
-        for (const packetData of packetsToSend) {
-            if (FCnt >= nPackets) {
-                break;
-            }
-            //encode the payload
-            const array = [parseFloat(packetData.soil_temp), parseFloat(packetData.soil_hum)]
-            const payload = Buffer.from(JSON.stringify(array));
-            const packet = packetGeneratorReal(DevAddr, AppSKey, NwkSKey, FPort, FCnt, packetData.timestamp, packetData.frequency, packetData.data_rate, packetData.coding_rate, packetData.gtw_channel, packetData.gtw_rssi, packetData.gtw_snr, payload);
-            const frameLoss = calculateLoss(); // Calculate frame loss using activeDevices
-            
-            sendPacketToAllGWs(packet, frameLoss, socket_arrays);
-            console.log(`Packet sent with DevAddr: ${DevAddr}, FCnt: ${FCnt}, Devices: ${activeDevices}`);
-            
-            FCnt++; // Increment FCnt for the next packet
-            await sleep(sleepTimer); 
-        }
-        
-        activeDevices--; // Decrement activeDevices when simulation is done
-        return resolve("Everything Alright");
-    });
-}
+          switch (chunk_index) {
+            case 0:
+              frameLosses = [0, 100];
+              break;
+            case 1:
+              let modifier = ((FCnt % chunk_len) * 100) / chunk_len;
+              if (modifier > 100) modifier = 100;
+              frameLosses = [modifier, 100 - modifier];
+              break;
+            case 2:
+              frameLosses = [100, 0];
+              break;
+            default:
+              frameLosses = calculateLoss();
+          }
+          break;
+        case -1: // COMPUTED
+          frameLosses = calculateLoss();
+          break;
+        default:
+          if (frameLoss_value < 0 || frameLoss_value > 100) {
+            return reject(
+              `Invalid frameLoss value: ${frameLoss_value}. It must be between 0 and 100.`
+            );
+          }
+          frameLosses = frameLoss_value;
+      }
 
-function main(){
-    // Read the JSON file containing gateway information
-    const gatewayData = JSON.parse(fs.readFileSync('experiment_files/gateways.json'));
-    // Read the JSON file containing experiment information
-    const experimentData = JSON.parse(fs.readFileSync('experiment_files/experiment.json'));
-    const experiment = experimentData.experiment[0]; // Assuming there's only one experiment in the array
-    const ratio = experiment.ratio;
-    const sleepTimer = experiment.sleepTimer;
-    const deviceTimer = experiment.deviceTimer;
-    const deviceNumber = experiment.deviceNumber;
-    const minPacket = experiment.minPacket;
-    const maxPacket = experiment.maxPacket;
-    const deviceList = experiment.deviceList;
-    // Read the JSON file containing device information
-    const deviceData = JSON.parse(fs.readFileSync(deviceList));
-    let promise_socket_arrays = [];
-    for (const gateway of gatewayData.gateways) {
-        promise_socket_arrays.push(socketCreator(gateway.host, gateway.port))
+      sendPacketToAllGWs(packet, frameLosses, socket_arrays);
+      console.log(
+        `Packet sent with DevAddr: ${DevAddr}, FCnt: ${FCnt}, Devices: ${activeDevices}`
+      );
+
+      FCnt++; // Increment FCnt for the next packet
+      await sleep(sleepTimer);
     }
-    Promise.all(promise_socket_arrays).then(async (socket_arrays) => {
-        let promise_device_arrays = []
-        currentRatio = 0;
-        const processedDevices = new Set();
-        for(const index in deviceData) {
-            const device = deviceData[index];
-            if (processedDevices.has(device.deviceid)) {
-                continue; // Skip if this deviceid has already been processed
-            }
-    
-            const DevAddr = device.deviceid;
-            const AppSKey = "18709C1192FEAA38F477BF6B0A6CB7E5";
-            const NwkSKey = "3FCAD3200F0FA7AA500A67AE5A72B1B0";
-            //const payload = device.soil_temp;
-            
-            // Ratio used is legacy/edge, it means 1 legacy every n edge devices
-            if (currentRatio === ratio) {
-                FPort = 2; // Legacy
-                currentRatio = 0;
-            } else {
-                FPort = 4; // Edge
-                currentRatio++;
-            }
-            const nPackets = Math.floor(Math.random() * (maxPacket - minPacket)) + minPacket;
-            //await sleep(deviceTimer);
-            promise_device_arrays.push(simulateDevice(DevAddr, AppSKey, NwkSKey, FPort, FCnt, sleepTimer, nPackets, socket_arrays,deviceList));
-            processedDevices.add(device.deviceid);
-        }
-        Promise.all(promise_device_arrays).then(() => {
-            console.log("Experiment Ended Successfully")
-        }).catch((err) => {
-            console.error(err)
-        }).finally(() => {
-            for(socket of socket_arrays){
-                socket.disconnect()
-            }
-        })
-    })
-    
+
+    activeDevices--; // Decrement activeDevices when simulation is done
+    return resolve("Everything Alright");
+  });
 }
+
+function main() {
+  // Read the JSON file containing gateway information
+  const gatewayData = JSON.parse(
+    fs.readFileSync("experiment_files/gateways.json")
+  );
+  // Read the JSON file containing experiment information
+  const experimentData = JSON.parse(
+    fs.readFileSync("experiment_files/experiment.json")
+  );
+  const experiment = experimentData.experiment[0]; // Assuming there's only one experiment in the array
+  const ratio = experiment.ratio;
+  const deviceNumber = experiment.deviceNumber;
+  const sleepTimer = experiment.sleepTimer;
+  const deviceTimer = experiment.deviceTimer;
+  const minPacket = experiment.minPacket;
+  const maxPacket = experiment.maxPacket;
+  const packetDataFilepath = experiment.packetData;
+  const frameLoss = experiment.frameLoss;
+  // Read the JSON file containing device information
+  const packetData = JSON.parse(fs.readFileSync(packetDataFilepath));
+  const deviceListpath = experiment.deviceList;
+  const deviceList = JSON.parse(fs.readFileSync(deviceListpath));
+
+  let formattedPacketData = [];
+  let processedDeviceId = [];
+  for (const packetInfo of packetData) {
+    if (!processedDeviceId.includes(packetInfo.deviceid)) {
+      let devicePackets = [];
+      for (const packetInfo2 of packetData) {
+        if (packetInfo2.deviceid === packetInfo.deviceid) {
+          devicePackets.push(JSON.parse(JSON.stringify(packetInfo2)));
+        }
+      }
+      formattedPacketData.push(devicePackets);
+      processedDeviceId.push(packetInfo.deviceid);
+    }
+  }
+
+  let promise_socket_arrays = [];
+  for (const gateway of gatewayData.gateways) {
+    promise_socket_arrays.push(socketCreator(gateway.host, gateway.port));
+  }
+  Promise.all(promise_socket_arrays).then(async (socket_arrays) => {
+    let promise_device_arrays = [];
+    currentRatio = 0;
+    for (const index in deviceList) {
+      if (index >= deviceNumber) {
+        break;
+      }
+      const deviceInfo = deviceList[index];
+
+      const DevAddr = deviceInfo.session.dev_addr;
+      const AppSKey = deviceInfo.session.keys.app_s_key.key;
+      const NwkSKey = deviceInfo.session.keys.f_nwk_s_int_key.key;
+      //const payload = device.soil_temp;
+      const packetsToSend =
+        formattedPacketData[index % formattedPacketData.length];
+
+      // Ratio used is legacy/edge, it means 1 legacy every n edge devices
+      if (currentRatio === ratio) {
+        FPort = 2; // Legacy
+        currentRatio = 0;
+        console.log("CREATING LEGACY DEVICE");
+      } else {
+        FPort = 4; // Edge
+        currentRatio++;
+        console.log("CREATING EDGE DEVICE");
+      }
+      const nPackets =
+        Math.floor(Math.random() * (maxPacket - minPacket)) + minPacket;
+
+      promise_device_arrays.push(
+        simulateDevice(
+          DevAddr,
+          AppSKey,
+          NwkSKey,
+          FPort,
+          FCnt,
+          sleepTimer,
+          nPackets,
+          frameLoss,
+          socket_arrays,
+          packetsToSend
+        )
+      );
+      await sleep(deviceTimer);
+    }
+    Promise.all(promise_device_arrays)
+      .then(() => {
+        console.log("Experiment Ended Successfully");
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        for (socket of socket_arrays) {
+          socket.disconnect();
+        }
+      });
+  });
+}
+
+// START MAIN
 main();
--- packetGenerator.js
+++ packetGenerator.js
@@ -1,62 +1,69 @@
 const lora_packet = require("lora-packet");
 
- function packetGenerator(DevAddr, AppSKey, NwkSKey, FPort, FCnt) {
-    // Construct the LoRa packet
-    const date = Date.now();
-    const constructedPacket = lora_packet.fromFields(
-      {
-        FPort: FPort, //FPort = 4 Device edge / FPort = 2 Device Legacy
-        MType: "Unconfirmed Data Up",
-        DevAddr: Buffer.from(DevAddr, "hex"),
-        FCtrl: {
-          ADR: false,
-          ACK: false,
-          ADRACKReq: false,
-          FPending: false,
-        },
-        FCnt: FCnt, //counter
-        payload: date.toString(), // Replace with your payload
-      },
-      Buffer.from(AppSKey, "hex"),
-      Buffer.from(NwkSKey, "hex")
-    );
-    const payloadBase64 = constructedPacket.getPHYPayload().toString("base64");
-    const size = payloadBase64.length;
-    let jsonUDP = {
-      rxpk: [
-        {
-          tmst: Math.floor(date/1000),
-          chan: 7,
-          rfch: 0,
-          freq: 868.1,
-          stat: 1,
-          modu: 'LORA',
-          datr: 'SF7BW125',
-          codr: '4/5',
-          lsnr: 9.2,
-          rssi: -33,
-          size: size,
-          data: payloadBase64,
-        }
-      ]
-    };
-    jsonPacket = JSON.stringify(jsonUDP);
-    /*headerPKTFWD[0] == PROTOCOL_VERSION == 2
-    headerPKTFWD[1] == numero random
-    headerPKTFWD[2] == numero random
-    headerPKTFWD[3] == PKT_PUSH_DATA == 0
-    headerPKTFWD[4] == net_mac_h = htonl((uint32_t)(0xFFFFFFFF & (lgwm>>32)));
-    headerPKTFWD[8] == net_mac_l = htonl((uint32_t)(0xFFFFFFFF &  lgwm  ));
-    lgwm == 0 Lora gateway MAC address
-    */
-    let headerPKTFWD = new Uint8Array([2, 45, 141, 0, 184, 39, 235, 255, 254, 230, 15, 44]);
-    let enc = new TextEncoder();
-    let json = enc.encode(jsonPacket);
+function packetGenerator(DevAddr, AppSKey, NwkSKey, FPort, FCnt) {
+  // Construct the LoRa packet
+  const date = Date.now().toString();
+  // encode the payload
+  const array = [
+    parseFloat(date.substring(date.length - 4, date.length - 2)),
+    parseFloat(date.substring(date.length - 2, date.length))
+  ];
+  const payload = Buffer.from(JSON.stringify(array));
+  const constructedPacket = lora_packet.fromFields({
+    FPort : FPort, // FPort = 4 Device edge / FPort = 2 Device Legacy
+    MType : "Unconfirmed Data Up",
+    DevAddr : Buffer.from(DevAddr, "hex"),
+    FCtrl : {
+      ADR : false,
+      ACK : false,
+      ADRACKReq : false,
+      FPending : false,
+    },
+    FCnt : FCnt,       // counter
+    payload : payload, // Replace with your payload
+  },
+                                                   Buffer.from(AppSKey, "hex"),
+                                                   Buffer.from(NwkSKey, "hex"));
+  const payloadBase64 = constructedPacket.getPHYPayload().toString("base64");
+  const size = payloadBase64.length;
+  let jsonUDP = {
+    rxpk : [ {
+      time : new Date().toISOString(),
+      tmst : Math.floor(date / 1000),
+      chan : 7,
+      rfch : 0,
+      freq : 868.1,
+      stat : 1,
+      modu : 'LORA',
+      datr : 'SF7BW125',
+      codr : '4/5',
+      lsnr : 9.2,
+      rssi : -33,
+      size : size,
+      data : payloadBase64,
+    } ]
+  };
+  jsonPacket = JSON.stringify(jsonUDP);
+  /*headerPKTFWD[0] == PROTOCOL_VERSION == 2
+  headerPKTFWD[1] == numero random
+  headerPKTFWD[2] == numero random
+  headerPKTFWD[3] == PKT_PUSH_DATA == 0
+  headerPKTFWD[4] == net_mac_h = htonl((uint32_t)(0xFFFFFFFF & (lgwm>>32)));
+  headerPKTFWD[8] == net_mac_l = htonl((uint32_t)(0xFFFFFFFF &  lgwm  ));
+  lgwm == 0 Lora gateway MAC address
+  */
+  let headerPKTFWD =
+      new Uint8Array([ 2, 45, 141, 0, 184, 39, 235, 255, 254, 230, 15, 44 ]);
+  let enc = new TextEncoder();
+  let json = enc.encode(jsonPacket);
 
-    // Create a new array with the total length and merge all source arrays.
-    let mergedArray = new Uint8Array(headerPKTFWD.length + json.length);
-    mergedArray.set(headerPKTFWD, 0); // Copy headerPKTFWD to mergedArray at the beginning.
-    mergedArray.set(json, headerPKTFWD.length); // Copy json to mergedArray after headerPKTFWD.   
-    return mergedArray;
+  // Create a new array with the total length and merge all source arrays.
+  let mergedArray = new Uint8Array(headerPKTFWD.length + json.length);
+  mergedArray.set(headerPKTFWD,
+                  0); // Copy headerPKTFWD to mergedArray at the beginning.
+  mergedArray.set(
+      json,
+      headerPKTFWD.length); // Copy json to mergedArray after headerPKTFWD.
+  return mergedArray;
 }
-module.exports = { packetGenerator };
+module.exports = {packetGenerator};
